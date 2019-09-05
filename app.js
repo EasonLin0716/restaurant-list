c2d541981e0dc5ba6fe762ec99680832b173a6cd
Eason Lin <uu9924079@gmail.com>
2019-09-05 20:12:01 +0800

    Add edit page and update action

--- app.js
+++ app.js
@@ -91,12 +91,31 @@ app.post('/restaurants', (req, res) => {
 
 // 修改 restaurant 頁面
 app.get('/restaurants/:id/edit', (req, res) => {
-  res.send('修改 restaurant 頁面')
+  Restaurants.findById(req.params.id, (err, restaurant) => {
+    if (err) return console.error(err)
+    return res.render('edit', { restaurant: restaurant })
+  })
 })
 
 // 修改 restaurant
 app.post('/restaurants/:id/edit', (req, res) => {
-  res.send('修改 restaurant')
+  console.log(req.body)
+  Restaurants.findById(req.params.id, (err, restaurant) => {
+    if (err) return console.error(err)
+    restaurant.name = req.body.name
+    restaurant.name_en = req.body.name_en
+    restaurant.category = req.body.category
+    restaurant.image = req.body.image
+    restaurant.location = req.body.location
+    restaurant.phone = req.body.phone
+    restaurant.google_map = req.body.google_map
+    restaurant.rating = req.body.rating
+    restaurant.description = req.body.description
+    restaurant.save(err => {
+      if (err) return console.error(err)
+      return res.redirect(`/restaurants/${req.params.id}`)
+    })
+  })
 })
 
 // 刪除 restaurants
