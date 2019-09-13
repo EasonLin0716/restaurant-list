33eb8bbe2ff84e8fb6c922d9d611144b97ab453f
Eason Lin <uu9924079@gmail.com>
2019-09-13 17:50:16 +0800

    rewrite seeder

--- models/seeds/restaurantListSeeder.js
+++ models/seeds/restaurantListSeeder.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
 // require restaurantList.js data
 const restaurantList = require('../restaurantList')
+const User = require('../user')
+const bcrypt = require('bcryptjs')
 
 // setting connection to mongoDB
 mongoose.connect('mongodb://localhost/restaurantList', { useNewUrlParser: true })
-
 
 const db = mongoose.connection
 
@@ -14,11 +15,49 @@ db.on('error', () => {
 
 // require json data
 const restaurantJson = require('./restaurant.json')
+const userJson = require('./user.json')
 
 
 db.once('open', () => {
   console.log('db connected!')
   // load json data into database
-  restaurantList.create(restaurantJson.results)
+  User.create(userJson.results).then(() =>
+
+    User.find({ email: 'user1@example.com' }, (err, user) => {
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(user[0].password, salt, (err, hash) => {
+          if (err) throw err
+          user[0].password = hash
+          user[0].save()
+        })
+      })
+
+      restaurantJson.results[0]["userId"] = user[0]._id.toString()
+      restaurantJson.results[1]["userId"] = user[0]._id.toString()
+      restaurantJson.results[2]["userId"] = user[0]._id.toString()
+    })
+
+  ).then(() =>
+
+    User.find({ email: 'user2@example.com' }, (err, user) => {
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(user[0].password, salt, (err, hash) => {
+          if (err) throw err
+          user[0].password = hash
+          user[0].save()
+        })
+      })
+
+      restaurantJson.results[3]["userId"] = user[0]._id.toString()
+      restaurantJson.results[4]["userId"] = user[0]._id.toString()
+      restaurantJson.results[5]["userId"] = user[0]._id.toString()
+    })
+
+  ).then(() =>
+
+    restaurantList.create(restaurantJson.results)
+
+  )
+
   console.log('Data create complete!')
 })
